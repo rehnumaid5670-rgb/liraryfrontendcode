e0761e13f6bd52c8addc2f1aad220ff06c0ca4ed
rehnuma mansuri <rehnumaid567.0@gmail.com>
2026-04-22 14:00:25 +0530

    Correcy API url

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://thisisfinalrepoofbackend.vercel.app/",
+  baseURL: "https://thisisfinalrepoofbackend.vercel.app/api",
   headers: {
     "Content-Type": "application/json"
   }
